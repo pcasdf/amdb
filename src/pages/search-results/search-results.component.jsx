542e955189e2dc743d65db7bf64c380244528a28
Peter Cho <pcho51990@gmail.com>
2020-06-05 16:51:12 -0400

    refactored search page

--- src/pages/search-results/search-results.component.jsx
+++ src/pages/search-results/search-results.component.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useCallback, useContext } from 'react';
 import axios from 'axios';
 import Loader from 'react-loader-spinner';
 
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 
 import { useStyles } from './search-results.styles';
 import { ThemeContext } from '../../contexts/theme/theme.context';
 import { ResultsContext } from '../../contexts/results/results.context';
 import Card from '../../components/card/card.component';
+import Sidebar from '../../components/sidebar/sidebar.component';
 
 const SearchResults = ({ match }) => {
   const [data, setData] = useState([]);
@@ -37,7 +38,7 @@ const SearchResults = ({ match }) => {
     setContext({ current: data });
   }, [data, setContext]);
 
-  const { body, head, gridHead } = useStyles();
+  const { body, head, content } = useStyles();
 
   return (
     <div className={body}>
@@ -50,12 +51,19 @@ const SearchResults = ({ match }) => {
           timeout={3000}
         />
       )}
-      <Grid container spacing={3}>
-        <Grid item md={12} className={gridHead}>
-          <span className={head}>Search results for "{title}"</span>
+      <Grid container>
+        <Hidden smDown>
+          <Grid item md={2}>
+            <Sidebar />
+          </Grid>
+        </Hidden>
+        <Grid container item xs={10} md={9} spacing={3} className={content}>
+          <Grid item xs={12}>
+            <span className={head}>Search results for "{title}"</span>
+          </Grid>
+          {data &&
+            data.map(item => <Card key={item.id} theme={theme} {...item} />)}
         </Grid>
-        {data &&
-          data.map(item => <Card key={item.id} theme={theme} {...item} />)}
       </Grid>
     </div>
   );
--- src/pages/search-results/search-results.styles.jsx
+++ src/pages/search-results/search-results.styles.jsx
@@ -2,13 +2,19 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles({
   body: {
-    width: '80%',
-    margin: '0 auto',
-    paddingTop: '100px'
+    width: '100%',
+    margin: '30px auto',
+    paddingTop: '60px'
   },
   head: {
     fontSize: '1.2rem',
     fontWeight: '500',
     color: '#126'
+  },
+  content: {
+    width: '80%',
+    '@media (max-width: 950px)': {
+      marginLeft: '10vw'
+    }
   }
 });
